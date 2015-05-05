54e2d051ae0821bd5cb227e397cd936cde291b47
emerset <emerset@gmail.com>
2015-05-05 13:07:38 -0400

    Customize static UI

--- posts.php
+++ posts.php
@@ -1,1 +1,15 @@
-<?php
+<?php include 'includes/header.php' ?>
+		  <div class="blog-post">
+            <h2 class="blog-post-title">International PHP Conference 2015</h2>
+            <p class="blog-post-meta">January 1, 2015 by <a href="#">Mark</a></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis nisl sed ex tempor, vitae vestibulum sem egestas. Mauris et eros consequat neque cursus pretium in nec justo. Donec suscipit magna eget mi egestas aliquam. Duis at cursus justo, et tincidunt tortor. Fusce id purus quis turpis placerat interdum. Ut semper tortor et vulputate pulvinar. Mauris posuere eu erat id mollis. Suspendisse elementum enim iaculis congue facilisis. Mauris urna velit, sagittis non tortor at, aliquam elementum orci. Nulla eget quam eu nibh tristique fringilla in vitae dui. Proin aliquam viverra est nec maximus. Morbi quis tortor congue libero pellentesque pulvinar vitae id nibh. Curabitur imperdiet odio ut sem aliquam iaculis. Donec dolor sapien, vehicula sed dui ut, fermentum vehicula ipsum. Aliquam eget mi nulla. Fusce luctus mollis urna, id molestie arcu dictum vitae. Proin placerat, nulla quis commodo facilisis, purus urna lobortis purus, ac varius velit risus eu tellus. Donec tempus arcu sem, mattis pharetra felis tempor nec. Suspendisse bibendum, ligula id pulvinar ornare, lectus quam sagittis mi, in porttitor sem justo sit amet ante.</p>
+          	<a class="readmore" href="post.php?id=1">Read More</a>
+          </div><!-- /.blog-post -->
+		  
+          <div class="blog-post">
+            <h2 class="blog-post-title">PHP 5.6.0beta4 Released</h2>
+            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis nisl sed ex tempor, vitae vestibulum sem egestas. Mauris et eros consequat neque cursus pretium in nec justo. Donec suscipit magna eget mi egestas aliquam. Duis at cursus justo, et tincidunt tortor. Fusce id purus quis turpis placerat interdum. Ut semper tortor et vulputate pulvinar. Mauris posuere eu erat id mollis. Suspendisse elementum enim iaculis congue facilisis. Mauris urna velit, sagittis non tortor at, aliquam elementum orci. Nulla eget quam eu nibh tristique fringilla in vitae dui. Proin aliquam viverra est nec maximus. Morbi quis tortor congue libero pellentesque pulvinar vitae id nibh. Curabitur imperdiet odio ut sem aliquam iaculis. Donec dolor sapien, vehicula sed dui ut, fermentum vehicula ipsum. Aliquam eget mi nulla. Fusce luctus mollis urna, id molestie arcu dictum vitae. Proin placerat, nulla quis commodo facilisis, purus urna lobortis purus, ac varius velit risus eu tellus. Donec tempus arcu sem, mattis pharetra felis tempor nec. Suspendisse bibendum, ligula id pulvinar ornare, lectus quam sagittis mi, in porttitor sem justo sit amet ante.</p>
+          	<a class="readmore" href="post.php?id=1">Read More</a>
+		  </div><!-- /.blog-post -->
+<?php include 'includes/footer.php' ?>
